18611f5ce163bba0c4932ebb07bb50fe3fde0cbc
Vladut Ilie <vladilie94@gmail.com>
2022-06-08 14:09:42 +0300

    Config types updated.

---
+++ src/types/Config.type.ts
@@ -0,0 +1,5 @@
+export type Config = {
+  merchantId: string;
+  secretKey: string;
+  testMode?: boolean;
+};
--- src/types/EuPlatescConfig.types.ts
+++
@@ -1,5 +0,0 @@
-export type EuPlatescConfig = {
-  merchantId: string;
-  privateKey: string;
-  testMode?: boolean;
-};
