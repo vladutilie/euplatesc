5854a7ecffa7d1097d9d90d0089c8e9bf8fbd367
Vladut Ilie <vladilie94@gmail.com>
2022-06-08 14:10:09 +0300

    Types index updated.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
-export { EuPlatescConfig } from './EuPlatescConfig.types';
+export { Config } from './Config.type';
+export { Hmac, Currency } from './Hmac.type';
+export {
+  Order,
+  BillingDetails,
+  ShippingDetails,
+  Extra,
+  BaseOrder,
+  BaseBillingDetails,
+  BaseShippingDetails,
+  BaseExtra
+} from './Order.type';
