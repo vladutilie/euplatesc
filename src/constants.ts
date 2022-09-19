81fbd24648d70ee52f4ed018bba9553257bd2f80
Vladut Ilie <vladilie94@gmail.com>
2022-09-19 22:46:32 +0300

    Files removed.

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const EUPLATESC_TEST_MERCHANT_ID = 'testaccount';
-export const EUPLATESC_TEST_SECRET_KEY = '00112233445566778899AABBCCDDEEFF';
-export const EUPLATESC_GATEWAY_URL = 'https://secure.euplatesc.ro/tdsprocess/tranzactd.php';
